30c96dad14cee1b6a0cc0b140e44fc811aed73d9
Paulo Henrique Rodrigues Pinheiro <paulohrpinheiro@gmail.com>
2017-02-10 20:33:41 -0200

    Add Post model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+User = settings.AUTH_USER_MODEL
+
+
+class Post(models.Model):
+    """Where content (blog posts) lives"""
+
+    pub_date = models.DateField(auto_now_add=True)
+    path = models.TextField(blank=False, unique=True)
+    title = models.CharField(max_length=70, blank=False)
+    subject = models.CharField(max_length=170, blank=False)
+    content = models.TextField(blank=False)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def  __str__(self):
+        """A post identifier"""
+        return self.path

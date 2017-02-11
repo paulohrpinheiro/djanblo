a230e638e9b73dc3f67081fb0f05e10863f90839
Paulo Henrique Rodrigues Pinheiro <paulohrpinheiro@gmail.com>
2017-02-11 18:31:17 -0200

    Change pud_date to DateTimeDate in post model

--- blog/models.py
+++ blog/models.py
@@ -8,7 +8,7 @@ User = settings.AUTH_USER_MODEL
 class Post(models.Model):
     """Where content (blog posts) lives"""
 
-    pub_date = models.DateField(auto_now_add=True)
+    pub_date = models.DateTimeField(auto_now_add=True)
     path = models.TextField(blank=False, unique=True)
     title = models.CharField(max_length=70, blank=False)
     subject = models.CharField(max_length=170, blank=False)

defbf11c8c92190aaf23dabdfbbd0e6b8483550f
Paulo Henrique Rodrigues Pinheiro <paulohrpinheiro@gmail.com>
2017-02-13 10:36:35 -0200

    Add auto fill for path field im admin

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from blog.models import Post
 
-admin.site.register(Post)
 
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"path": ("title",)}
+    fields = ('title', 'path', 'subject', 'content', 'author')

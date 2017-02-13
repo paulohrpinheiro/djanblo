6fe4cc82d6975de775cf86b0a3f04dd7ce73ccee
Paulo Henrique Rodrigues Pinheiro <paulohrpinheiro@gmail.com>
2017-02-12 22:19:32 -0200

    Move to_json function to utils "package"

---
+++ util/json.py
@@ -0,0 +1,7 @@
+import json
+
+
+def to_json(from_string):
+    """Convert string to json."""
+    # Only python >= 3.6 accept str & bytes in json.loads
+    return json.loads(str(from_string, 'utf-8'))
